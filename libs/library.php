9489ca2a5c9858cbab3466341ed2db1612d36552
Sebastian Fasold-Schmid <sebastian@fasold-schmid.com>
2019-08-20 20:26:01 +0200

    fix

--- libs/library.php
+++ libs/library.php
@@ -48,7 +48,6 @@ trait RobonectLibrary
 	}
 	
     public function GetStatus($url, $debug = false){
-    {
 		$ip = $this->ReadPropertyString('ip');
 		$user = $this->ReadPropertyString('user');
         $password = $this->ReadPropertyString('password');
@@ -126,62 +125,9 @@ trait RobonectLibrary
 		$context = stream_context_create($opts);
 		$data = file_get_contents($url, false, $context);
 		return $data;
-	}
-
-    private function getToken()
-    {
-        $user = $this->ReadPropertyString('user');
-        $password = $this->ReadPropertyString('password');
-
-        $dtoken = $this->GetBuffer('Token');
-        $jtoken = json_decode($dtoken, true);
-        $token = isset($jtoken['token']) ? $jtoken['token'] : '';
-        $provider = isset($jtoken['provider']) ? $jtoken['provider'] : '';
-        $expiration = isset($jtoken['expiration']) ? $jtoken['expiration'] : 0;
-
-        if ($expiration < time()) {
-            $postdata = [
-                    'data' => [
-                        'attributes' => [
-                            'username' => $user,
-                            'password' => $password
-                        ],
-                        'type' => 'token'
-                    ]
-                ];
-
-            $header = [
-                    'Accept: application/json',
-                    'Content-Type: application/json'
-                ];
-
-            $ctoken = $this->do_HttpRequest($this->url_im . 'token', $header, $postdata, true);
-            $this->SendDebug(__FUNCTION__, 'ctoken=' . print_r($ctoken, true), 0);
-            if ($ctoken == '') {
-                return false;
-            }
-            $jtoken = json_decode($ctoken, true);
-            $this->SendDebug(__FUNCTION__, 'jtoken=' . print_r($jtoken, true), 0);
-
-            $token = $jtoken['data']['id'];
-            $provider = $jtoken['data']['attributes']['provider'];
-            $expires_in = $jtoken['data']['attributes']['expires_in'];
-            $user_id = $jtoken['data']['attributes']['user_id'];
-
-            $jtoken = [
-                    'token'            => $token,
-                    'provider'         => $provider,
-                    'user_id'          => $user_id,
-                    'expiration'       => time() + $expires_in
-                ];
-            $this->SetBuffer('Token', json_encode($jtoken));
-        }
-
-        return $jtoken;
     }
 
-    private function do_ApiCall($url, $postdata = '')
-    {
+    private function do_ApiCall($url, $postdata = '') {
         $inst = IPS_GetInstance($this->InstanceID);
         if ($inst['InstanceStatus'] == IS_INACTIVE) {
             $this->SendDebug(__FUNCTION__, 'instance is inactive, skip', 0);
@@ -208,8 +154,7 @@ trait RobonectLibrary
         return $cdata;
     }
 
-    private function do_HttpRequest($url, $header = '', $postdata = '')
-    {
+    private function do_HttpRequest($url, $header = '', $postdata = '') {
         $req = $postdata != '' ? 'post' : 'get';
 
         $this->SendDebug(__FUNCTION__, 'http-' . $req . ': url=' . $url, 0);
